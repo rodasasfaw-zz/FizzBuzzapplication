6dfe7c8b0130e6992ce2568765cec97a65144b17
rodasasfaw <rodasasfaw4@gmail.com>
2018-01-26 09:10:23 -0500

    gfgfdgdf

--- src/main/java/fizzbuzz/application/MainController.java
+++ src/main/java/fizzbuzz/application/MainController.java
@@ -1,30 +1,51 @@
 package fizzbuzz.application;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import java.util.Scanner;
+import java.util.Scanner;
 @RestController
 public class MainController {
 
-    @RequestMapping("/")
-    public String showIndex() {
-        int i;
-        String numberasstring ="";
+@RequestMapping("/")
+    public String show() {
+        System.out.println("input the ending number here: ");
 
-        for (i = 1; i <= 90; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
-            } else {
-                System.out.println(i);
-            }
-            numberasstring = String.valueOf(i);
+        Scanner sc = new Scanner(System.in);
+        int x= sc.nextInt();
+        
+        String message="";
+        for(int i=1; i<=x;i++) {
+            if(i%5==0&&i%3==0)
+                message= message + "FizzBuzz<br/>";
+
+            else if(i%3==0)
+                message= message + "Fizz<br/>";
+            else if(i%5==0)
+                message= message + "Buzz<br/>";
+
+            /*else if(i%7==0)
+                count_7++;
+
+            else if(i%17==0)
+                count_17++;
+            else if(i%23==0)
+                count_23++;
+
+    */
+
+            else
+                message= message + Integer.toString(i)+"<br/>";
 
 
-        }
-
-        return numberasstring;
+       }
+       /*
+        System.out.println("the number of mulfiple of 7 is:"+count_7);
+        System.out.println("the number of mulfiple of 17 is:"+count_17);
+        System.out.println("the number of mulfiple of 23 is:"+count_23);
+*/
+return message;
     }
+
+
+
 }
+
